40f0a8306ade84777495b7be72fed9d1f5732543
harunalbayrak <harunnalbayrakk278@gmail.com>
2021-02-06 17:01:08 +0300

    Add searching book with title

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -2,12 +2,14 @@ class Book{
   final String _title;
   final String _thumbnail;
   final String _author;
+  final String _description;
   final int _page;
 
-  Book(this._title, this._thumbnail, this._author, this._page);
+  Book(this._title, this._thumbnail, this._author, this._description, this._page);
 
   String get title => _title;
   String get thumbnail => _thumbnail;
   String get author => _author;
+  String get description => _description;
   int get page => _page;
 }
--- lib/services/book_api.dart
+++ lib/services/book_api.dart
@@ -2,14 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:donence_app/models/book.dart';
 
 class BookAPI{
-  static final String url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
+  static final String urlIsbn = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
+  static final String urlSearch = 'https://www.googleapis.com/books/v1/volumes?q=';
+  static final String api_key = 'AIzaSyBKPrJ8ZNhvZj4WrmMAPC4at82sRkXdUN4';
 
   static Future<Book> getIsbnBook(String isbn) async{
-    var responseUrl = url + isbn;
+    var responseUrl = urlIsbn + isbn;
     var response = await getResponse(responseUrl);
+
     var title = '';
     var thumbnail = '';
     var author = '';
+    var description = '';
     var page = 0;
 
     try{
@@ -18,11 +22,44 @@ class BookAPI{
       title = volumeInfo['title'];
       thumbnail = volumeInfo['imageLinks']['thumbnail'];
       author = volumeInfo['authors'][0];
+      description = volumeInfo['description'];
       page = volumeInfo['pageCount'];
 
-      return Book(title,thumbnail,author,page);
+      return Book(title,thumbnail,author,description,page);
     } catch(e){
       print(e);
+      return null;
+    }
+  }
+
+  static Future<List<Book>> getSearchBooks(String search) async{
+    var responseUrl = urlSearch + search + '+intitle:keyes&key=' + api_key;
+    var response = await getResponse(responseUrl);
+
+    List items = response.data['items'];
+
+    var listBooks = <Book>[];
+
+    var title = '';
+    var thumbnail = '';
+    var author = '';
+    var description = '';
+    var page = 0;
+
+    try {
+      for (var i = 0; i < items.length; ++i) {
+        var volumeInfo = response.data['items'][i]['volumeInfo'];
+
+        title = volumeInfo['title'];
+        thumbnail = volumeInfo['imageLinks']['thumbnail'];
+        author = volumeInfo['authors'][0];
+        description = volumeInfo['description'];
+        page = volumeInfo['pageCount'];
+
+        listBooks.add(Book(title, thumbnail, author, description, page));
+      }
+      return listBooks;
+    } catch(e){
       return null;
     }
   }
