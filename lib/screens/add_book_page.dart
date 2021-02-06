dc9e5a1a3d3f67c9a928dd612b9d7ff63382dc97
H. İbrahim İLHAN <halil000ibrahim@gmail.com>
2021-02-07 01:49:45 +0300

    added image pick to form page

--- lib/screens/add_book_page.dart
+++ lib/screens/add_book_page.dart
@@ -1,25 +1,35 @@
-import 'package:donence_app/services/book_api.dart';
 import 'package:flutter/material.dart';
 import 'package:donence_app/models/book.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddBookPage extends StatefulWidget {
-  final Book book;
-
-  const AddBookPage({Key key, this.book}) : super(key: key);
   @override
   _AddBookPageState createState() => _AddBookPageState();
 }
 
 class _AddBookPageState extends State<AddBookPage> {
   final _formKey = GlobalKey<FormState>();
-  String isbn;
-  String title;
-  String author;
-  String publisher;
-  String publish_date;
-  String comment;
-  bool addDonation;
-  //image cover;
+  String _title;
+  String _thumbnail;
+  String _author;
+  String _description;
+  int _page;
+  String _isbn13;
+  String _publisher;
+  String _publish_date;
+  String _comment;
+
+  final picker = ImagePicker();
+
+  _getImage(ImageSource imageSource) async {
+    PickedFile imageFile = await picker.getImage(source: imageSource);
+    if (imageFile == null) return;
+    setState(
+      () {
+        _thumbnail = imageFile.path;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,126 +38,210 @@ class _AddBookPageState extends State<AddBookPage> {
         title: Text('Add Book'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            _bookForm(),
-            _formSaveButton()
-          ],
+      body: _bookForm(),
+      bottomNavigationBar: _formSaveButton(context),
+    );
+  }
+
+  Widget _bookForm() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Title';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _title = val;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Author',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Author';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _author = val;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'ISBN',
+                ),
+                validator: (value) {
+                  if (value.length != 13 && value.length != 10) {
+                    return 'Invalid ISBN';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _isbn13 = val;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Page',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Page';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _page = int.parse(val);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Publisher',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Publisher';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _publisher = val;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Publish Year',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Publish Year';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _publish_date = val;
+                },
+              ),
+              TextFormField(
+                minLines: 2,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  labelText: 'Comment',
+                  icon: Icon(Icons.comment),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Comment';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  _comment = val;
+                },
+              ),
+              Padding(padding: EdgeInsets.all(16)),
+              Row(
+                children: [
+                  SizedBox(
+                    child:
+                        Image.asset(_thumbnail ?? 'assets/images/placeholder.png'),
+                    width: MediaQuery.of(context).size.width / 5,
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      leading: Icon(Icons.add_photo_alternate_outlined),
+                      trailing: Icon(Icons.arrow_drop_down),
+                      title: Text('Cover (optional)'),
+                      onTap: () {
+                        onCoverPressed();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _bookForm(){
-    return Form(
-        key: _formKey,
-        child: Column(children: <Widget>[
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'ISBN (optional)',
+  void onCoverPressed() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Colors.blueGrey,
+            height: 120,
+            child: Container(
+              child: _buildCameraMenu(),
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(10),
+                  topRight: const Radius.circular(10),
+                ),
+              ),
             ),
-            validator: (value) {
-              if (value.length != 13 || value.length != 10) {
-                return 'ISBN number must be 13 characters';
-              }
-              return null;
-            },
-            onSaved: (val){
-              isbn = val;
-            },
-          ),
-          TextFormField(
-            initialValue: title,
-            decoration: InputDecoration(
-              labelText: 'Title',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter Title';
-              }
-              return null;
-            },
-            onSaved: (val){
-              title = val;
-            },
-          ),
-          TextFormField(
-            //initialValue: _book.title,
-            decoration: InputDecoration(
-              labelText: 'Author',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter Author';
-              }
-              return null;
-            },
-            onSaved: (val){
-              author = val;
-            },
-          ),
-          TextFormField(
-            //initialValue: _book.title,
-            decoration: InputDecoration(
-              labelText: 'Publisher',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter Publisher';
-              }
-              return null;
-            },
-            onSaved: (val){
-              publisher = val;
-            },
-          ),
-          TextFormField(
-            //initialValue: _book.title,
-            decoration: InputDecoration(
-              labelText: 'Publisher',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter Publisher';
-              }
-              return null;
-            },
-            onSaved: (val){
-              publish_date = val;
-            },
-          ),
-
-
-        ]));
+          );
+        });
   }
 
-  Widget _bookFormField(String title, val){
-
+  Column _buildCameraMenu() {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.image),
+          title: Text('Select from gallery'),
+          onTap: () => _getImage(ImageSource.gallery),
+        ),
+        ListTile(
+          leading: Icon(Icons.camera_alt),
+          title: Text('Take a picture'),
+          onTap: () => _getImage(ImageSource.camera),
+        ),
+      ],
+    );
   }
 
-  Widget _formSaveButton() {
+  Widget _formSaveButton(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
         onPressed: () {
           if (_formKey.currentState.validate()) {
-            Scaffold.of(context)
-                .showSnackBar(SnackBar(content: Text('Processing Data')));
-            print('isbn $isbn');
-            print('title $title');
-            print('author $author');
-            print('publisher $publisher');
-            print('publishDate $publish_date');
-            print('comment $comment');
-            print('addDonation $addDonation');
+            _formKey.currentState.save();
+            print(
+                'BOOK => ${Book(_title, _thumbnail, _author, _description, _page, _isbn13, _publisher, _publish_date).toMap()}');
+            print('COMMENT => $_comment');
+
+            //TODO: store book to database
+
+            Navigator.pop(context);
           }
         },
-        child: Text(
-          'SAVE',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'SAVE',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
