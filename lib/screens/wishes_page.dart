d9d41fe0f3854de21fe64b6b5efe1e41ff9fb3ad
harunalbayrak <harunnalbayrakk278@gmail.com>
2021-02-07 12:50:59 +0300

    Change style of wishes page

--- lib/screens/wishes_page.dart
+++ lib/screens/wishes_page.dart
@@ -1,9 +1,11 @@
 import 'package:donence_app/models/book.dart';
 import 'package:donence_app/services/database_service.dart';
-import 'package:donence_app/widget/book_list_tile_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+
+import 'package:random_color/random_color.dart';
 
 class WishesPage extends StatefulWidget {
   @override
@@ -30,14 +32,14 @@ class _WishesPageState extends State<WishesPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: StreamBuilder(
+        child: StreamBuilder(
           stream: ref.onValue,
           builder: (BuildContext context, snapshot) {
-            if (snapshot.hasError) {
+            if(snapshot.hasError){
               return Text('Error');
-            } else if (snapshot.data == null) {
-              return Text('Data == null');
-            } else {
+            } else if(snapshot.data == null){
+              return Text('');
+            } else{
               var listBook = <Book>[];
 
               Map data = snapshot.data.snapshot.value;
@@ -45,36 +47,50 @@ class _WishesPageState extends State<WishesPage> {
                 String name = key;
                 Map val = value;
 
-                val.forEach((key, value) {
-                  String title = value['title'] ?? '';
-                  String thumbnail = value['thumbnail'] ?? '';;
-                  String author = value['author'] ?? '';;
-                  var description = name ?? '';;
-                  int page = value['page'] ?? 0;
-                  String isbn13 = value['isbn13'] ?? '';
-                  ;
-                  String publisher = value['publisher'] ?? '';
-                  ;
-                  String publish_date = value['publish_date'] ?? '';
-                  ;
+                if (FirebaseAuth.instance.currentUser.displayName == name){
+                  val.forEach((key, value) {
+                    String title = value['title'] ?? '';
+                    String thumbnail = value['thumbnail'] ?? '';;
+                    String author = value['author'] ?? '';;
+                    String description = name ?? '';;
+                    int page = value['page'] ?? 0;
+                    String isbn13 = value['isbn13'] ?? '';;
+                    String publisher = value['publisher'] ?? '';;
+                    String publish_date = value['publish_date'] ?? '';;
 
-                  var x = Book(title, thumbnail, author, description, page,
-                      isbn13, publisher, publish_date);
-                  listBook.add(x);
-                });
+                    var x = Book(title,thumbnail,author,description,page,isbn13,publisher,publish_date);
+                    listBook.add(x);
+                  });
+                }
               });
               return ListView.builder(
                 itemCount: listBook.length,
                 itemExtent: 120,
                 itemBuilder: (BuildContext context, int index) {
-                  return BookListTileWidget(
-                    book: listBook[index],
-                    onPressed: () => {},
+                  return Card(
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          border: Border(right: BorderSide(color: RandomColor().randomColor(), width: 5))
+                      ),
+                      child: Center(
+                        child: ListTile(
+                          leading: Image.network(
+                            listBook[index].thumbnail,
+                            fit: BoxFit.fitHeight,
+                          ),
+                          title: Text(listBook[index].title),
+                          subtitle: Text(listBook[index].author),
+                          onTap: () => {},
+                        ),
+                      ),
+                    ),
                   );
                 },
               );
             }
-          }),
+          }
+        ),
     );
   }
 }
